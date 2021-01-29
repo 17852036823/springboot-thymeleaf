40bef72ac2dfd1eb443238f305a231578bd31c53
khy520 <1648966207@qq.com>
2021-01-29 08:23:32 +0800

    第一次创建方法，编写测试方法

---
+++ src/main/java/com/offcn/controller/Test01Controller.java
@@ -0,0 +1,20 @@
+package com.offcn.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author： xiaoyu
+ * @date： 2021/1/29 8:20
+ * @version：1.0
+ * @Description：
+ */
+@RestController
+@RequestMapping("/test")
+public class Test01Controller {
+
+    public void test(){
+
+    }
+
+}
